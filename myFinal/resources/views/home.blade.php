63259664bb5d61bf9979f08dabd6f58b1c250186
Kita <ankitashah212@gmail.com>
2017-11-03 12:26:25 -0400

    dashboard added

--- myFinal/resources/views/home.blade.php
+++ myFinal/resources/views/home.blade.php
@@ -4,7 +4,9 @@
 <div class="container">
     <div class="row new-post">
        <div class="col-md-6 col-offset-3">
-           
+           <header>
+               <h2>New Post</h2>
+           </header>
        </div>
     </div>
 </div>
