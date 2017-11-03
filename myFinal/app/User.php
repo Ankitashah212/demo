4e56a3e2bb8a954c0051c58907b8a0a713584185
Kita <ankitashah212@gmail.com>
2017-11-03 15:04:27 -0400

    taking a break

--- myFinal/app/User.php
+++ myFinal/app/User.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+
+  
     use Notifiable;
 
     /**
@@ -26,4 +28,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+   
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
+   
 }
